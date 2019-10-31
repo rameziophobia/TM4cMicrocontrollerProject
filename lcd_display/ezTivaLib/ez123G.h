dec5e6d7eb47bd7c4c9ca14e80f09669ba5e776a
Ramez Noshy <RamezNoshy@gmail.com>
2019-11-01 01:59:09 +0200

    add lcd_display project

---
+++ lcd_display/ezTivaLib/ez123G.h
@@ -0,0 +1,14 @@
+#ifndef __EZ123G_LIB__H
+#define __EZ123G_LIB__H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "ezDefines.h"
+#include "ezTimer.h"
+#include "ezLCD.h"
+
+#endif
+
+
